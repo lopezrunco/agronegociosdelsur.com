5c9206a49b9165b08ac43cdff7b2ab5c45109ea3
lopezrunco <lopezrunco@gmail.com>
2026-01-13 10:09:21 -0300

    feat: Add contact page.

---
+++ contact.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Template Name: Contact
+ * 
+ * @package ANS WP Theme
+ */
+
+get_header();
+?>
+
+<section>
+    <article class="container">
+        <div class="row">
+            <div class="col-lg-5 mb-5">
+                <?php get_template_part('template-parts/contact-info'); ?>
+            </div>
+            <div class="col-lg-7 mb-5 mb-lg-0">
+                <div class="section-title">
+                    <h2>Envíenos un mensaje</h2>
+                    <div class="separator"></div>
+                </div>
+                <?php
+                // Dev form
+                echo do_shortcode('[contact-form-7 id="5f9fe5f" title="Formulario de contacto"]');
+
+                // Prod form
+                // echo do_shortcode('[contact-form-7 id="63d971a" title="Formulario de contacto"]'); 
+                ?>
+            </div>
+            <div class="col-12">
+                <?php get_template_part('template-parts/social-icons'); ?> 
+            </div>
+        </div>
+    </article>
+</section>
+
+<?php 
+get_template_part('template-parts/map');
+get_footer();
--- template-parts/map.php
+++ template-parts/map.php
@@ -1,3 +1,11 @@
-<div class="bg-primary">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d25517.552489363698!2d-56.166678735456934!3d-34.89036941498789!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x959f80ffc63bf7d3%3A0x6b321b2e355bec99!2sMontevideo%2C%20Departamento%20de%20Montevideo%2C%20Uruguay!5e0!3m2!1ses-419!2sin!4v1717759775551!5m2!1ses-419!2sin" width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+<div class="bg-light">
+    <iframe 
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6636.621058899224!2d-56.32724912744264!3d-33.726785465563005!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a12f4f7e14427d%3A0x33c53e9fc82c693!2sAgronegocios%20del%20sur!5e0!3m2!1ses-419!2suy!4v1768308309284!5m2!1ses-419!2suy" 
+        width="100%" 
+        height="500" 
+        style="border:0;" 
+        allowfullscreen="" 
+        loading="lazy" 
+        referrerpolicy="no-referrer-when-downgrade">
+    </iframe>
 </div>
